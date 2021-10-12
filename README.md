b6725d12659d644e2e4e1852cab19ced2e9b3535
rosedunlop <rosedunlop@outlook.com>
2021-10-12 13:44:12 +0100

    third submit 12/10/21

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Froggy
+
 Save Poppy The Puppy: This is my first project from the General Assembly Course SEI-59
+
+To begin with I planned out my pseudo code for the HTML, JavaScript and CSS.
+
+The layout of the HTML was quite basic, it has a header, a main containing two divs - one for the game specifics which is on the left hand side of the page and the game content which is situated on the right.
+The game content is where the 10 by 10 grid is kept - where I made 100 divs in the HTML document itself.
--- app.js
+++ app.js
@@ -17,7 +17,7 @@ rowFour = changingRows.map((element, index) =>{
     if(index % 2 === 0){
         return 'river'
     }
-    return 'rubber-dingy'
+    return 'rubber-dingy' 
 })
 console.log(rowFour)
 
@@ -61,77 +61,78 @@ const fifthRow = document.querySelectorAll('.row-five')
 const sixthRow = document.querySelectorAll('.row-six')
 const seventhRow = document.querySelectorAll('.row-seven')
 
-const changeFourthRow = setInterval(() => {
-    fourthRow.forEach(
-        (cell) => {
-            if(cell.classList.contains('rubber-dingy')){
-                cell.classList.remove('rubber-dingy')
-                cell.classList.add('river')
-            }else if(cell.classList.contains('river')){
-                cell.classList.remove('river')
-                cell.classList.add('rubber-dingy')
-            }
+const moveDingys = 
+    (cell) => {
+        cell.classList.add('river')
+        cell.classList.remove('rubber-dingy')
+          // if it contains player - move with the dingy when it does move
+          const currentIndex = Array.from(cells).indexOf(cell)
+          // If the cell contains the player - remove it from the current cell and add it along 1. 
+            if(cell.classList.contains('puppy')){
+                console.log('rubber dingy encountered')
+                console.log(currentIndex)
+                cell.classList.remove('puppy')
+                cells[currentIndex + 1].classList.add('puppy')
+                // If dingy reaches end of the row, player dies/loses a life
+                if(currentIndex === 39 || 49 || 59 || 69){
+                    if (lives > 0) {
+                        lives -= 1
+                        grabLives.textContent = `${lives}`
+                        cells[currentIndex].classList.remove('puppy')
+                        cells[95].classList.add('puppy')
+                    }else{
+                       grabResult.textContent = 'Game Over.'
+                       cells[currentIndex].classList.remove('puppy')
+                       alert('Game Over.')         
+                    }
+                }   
+            } 
+            // Add the rubber dingy plus one along the row too 
+            cells[currentIndex + 1].classList.add('rubber-dingy')
+            // if dingy reaches end of row need to add dingy at start of that row again to keep regenerating them 
+            if (currentIndex === 39 ){
+                cells[30].classList.add('rubber-dingy')
+            }else if(currentIndex === 49){
+                cells[40].classList.add('rubber-dingy')
+            }else if(currentIndex === 59){
+                cells[50].classList.add('rubber-dingy')
+            }else{
+                return
+            }        
         }   
-    )  
+    
+
+const isADingy = (cell) => {
+    return cell.classList.contains('rubber-dingy')
+}  
+const moveDingysOnRow = (row) => {
+    Array.from(row).filter(isADingy).forEach(moveDingys)
+}
+
+const changeFourthRow = setInterval(() => {
+    moveDingysOnRow(fourthRow) 
 }, 2000)
 
 const changeFifthRow = setInterval(() => {
-    fifthRow.forEach(
-        (cell) => {
-            if(cell.classList.contains('rubber-dingy')){
-                cell.classList.remove('rubber-dingy')
-                cell.classList.add('river')
-            }else if(cell.classList.contains('river')){
-                cell.classList.remove('river')
-                cell.classList.add('rubber-dingy')
-            }
-        }   
-    )  
+    moveDingysOnRow(fifthRow)  
 }, 1500)
 
 const changeSixthRow = setInterval(() => {
-    sixthRow.forEach(
-        (cell) => {
-            if(cell.classList.contains('rubber-dingy')){
-                cell.classList.remove('rubber-dingy')
-                cell.classList.add('river')
-            }else if(cell.classList.contains('river')){
-                cell.classList.remove('river')
-                cell.classList.add('rubber-dingy')
-            }
-        }   
-    )  
+    moveDingysOnRow(sixthRow)  
 }, 750)
 
 let index = 30 
 const changeSeventhRow = setInterval(() => {
-            // if(index > 29 && index <= 39){
-            //     if(cells[39].classList.contains('rubber-dingy')){
-            //         cells[39].classList.remove('rubber-dingy')
-            //         cells[39].classList.add('river')
-            //         cells[30].classList.add('rubber-dingy')
-            //         index = 30
-            //     }else if(cells[index].classList.contains('rubber-dingy')){
-            //         cells[index].classList.remove('rubber-dingy')
-            //         index += 1
-            //         cells[index].classList.add('rubber-dingy')
-            //     }
-            // }
-
-            seventhRow.forEach(
-                (cell) => {
-                    if(cell.classList.contains('rubber-dingy')){
-                        cell.classList.remove('rubber-dingy')
-                        cell.classList.add('river')
-                    }else if(cell.classList.contains('river')){
-                        cell.classList.remove('river')
-                        cell.classList.add('rubber-dingy')
-                    }
-
-                }
-            )                 
+    moveDingysOnRow(seventhRow)               
 }, 2500)
 
+let startingPosition = 95
+console.log(startingPosition)
+ // Here I have declared a function to add the puppy class to that starting position cell
+const addPuppyAtStart = () => {
+    cells[startingPosition].classList.add('puppy')
+}
+addPuppyAtStart()
 
 cells[10].classList.add('puppy-snatcher')
 let i = 10
@@ -141,7 +142,17 @@ const moveSnatcher = () => {
               cells[29].classList.remove('puppy-snatcher')
               cells[10].classList.add('puppy-snatcher')
               i = 10
-          }else{
+          }else if(cells[i].classList.contains('puppy') && lives > 0){
+              lives -= 1
+              grabLives.textContent = `${lives}`
+              grabResult.textContent = `You lost a life! You have ${lives} lives left.`
+              cells[i].classList.remove('puppy')
+              cells[95].classList.add('puppy')
+          } else if(cells[i].classList.contains('puppy') && lives === 0){
+            grabResult.textContent = 'Game Over.'
+            alert('Game Over.')
+          }
+          else{
               cells[i].classList.remove('puppy-snatcher')
               i += 1
               cells[i].classList.add('puppy-snatcher')
@@ -153,13 +164,6 @@ setInterval(moveSnatcher, 500)
 
 
 // Here I am declaring where the player will start - in a let as it needs to be able to change:
-let startingPosition = 95
-console.log(startingPosition)
- // Here I have declared a function to add the puppy class to that starting position cell
-const addPuppyAtStart = () => {
-    cells[startingPosition].classList.add('puppy')
-}
-addPuppyAtStart()
 
 
 // Now when a key is pressed and I want the player to move I will create a function where it removes from the old position to new one:
@@ -176,6 +180,7 @@ const grabLives = document.querySelector('span')
 grabLives.textContent = `${lives}`
 const grabResult = document.querySelector('.result')
 
+
 const movePlayer = (changeInIndex, isIndexAtLimit) => {
     const newIndex = startingPosition + changeInIndex
     if(isIndexAtLimit(startingPosition)){
@@ -183,20 +188,20 @@ const movePlayer = (changeInIndex, isIndexAtLimit) => {
         return
     }
     const newCell = cells[newIndex]
-    
     // Obstacles that stop player from moving
     if(newCell.classList.contains('obstacle')){
         console.log('obstacle encountered')
         if(lives > 0){
           lives -= 1
           grabLives.textContent = `${lives}`
-          grabResult.textContent = `You lost a life! You have ${lives} lives left.` 
+          grabResult.textContent = `You lost a life! You have ${lives} lives left.`
+
         } else {
             grabResult.textContent = 'Game over.'
             alert('Game Over!')
         }
         return
-    }else if(newCell.classList.contains('river')){
+    }else if(newCell.classList.contains('river') && !newCell.classList.contains('rubber-dingy')){
         console.log('river encountered')
         if(lives > 0){
            lives -= 1
@@ -220,13 +225,18 @@ const movePlayer = (changeInIndex, isIndexAtLimit) => {
         return
     } else if (newCell.classList.contains('rubber-dingy')){
         console.log('rubber dingy encountered')
-        move(newIndex + 2)
         console.log(newIndex)
+        console.log(newCell)
+        const moveWithDingy = newCell[newIndex + 1]
+        console.log(moveWithDingy)
+    
     }
 
     console.log(startingPosition, newIndex)
     move(newIndex)
 }
+
+
 
 // I would make four functions for each movement:
     const handleArrowUp = () => {
